a5bda03ed2639b1502b1d085d8fa68434abd3ffd
Slade Sawyer <Slade@Slades-MacBook-Air.local>
2019-06-09 19:17:45 -0400

    created routes for account

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const accountController = require('../controllers/accountController.js')
+const eventController = require('../controllers/eventController.js')
+const wagerController = require('../controllers/wagerController.js')
+const apiController = require('../controllers/apiController.js')
+
+//  Account CRUD routes
+
+//  Create: creates or logs in a user
+router.patch("/account/login", accountController.loginOrCreate)
+
+//  Loads landing page, should find a better place than this file
+router.get("/", accountController.showLandingPage)
+
+//  Read: all accounts
+router.get("/accounts", accountController.findAccountByName)
+
+//  Read: single account
+router.get("/account", accountController.findAccountByName)
+
+//  Update: updates name
+router.patch("account/updateName", accountController.updateAccountName)
+
+//  Update: updates balance
+router.patch("account/updateBalance", accountController.updateAccountBalance)
+
+//  Delete: deletes single user
+router.delete("account", accountController.deleteAccount)
+
+
+
+
